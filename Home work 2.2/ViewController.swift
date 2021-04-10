b6e741dbcab9eab0d3a919ba14e8bbffd9a03110
Nika Rakhmanova <nikalaus28@gmail.com>
2021-04-10 18:49:18 +0300

    Initial Commit

---
+++ Home work 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2.2
+//
+//  Created by Nika Rakhmanova on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
